00c2aee96514171edd52a2a6542cf771dc06cd6f
Sven Erik Matzen <sven.matzen@sdx-ag.de>
2020-01-20 18:16:40 +0100

    added version and license information

--- Sem.Tools.Logging/LogScope.cs
+++ Sem.Tools.Logging/LogScope.cs
@@ -1,36 +1,54 @@
-﻿using System.Threading.Tasks;
-
-namespace Sem.Tools.Logging
+﻿namespace Sem.Tools.Logging
 {
     using System;
+    using System.Collections.Concurrent;
+    using System.Globalization;
     using System.Runtime.CompilerServices;
+    using System.Threading.Tasks;
 
     public class LogScope : IDisposable, IAsyncDisposable
     {
         private readonly DateTime start = DateTime.UtcNow;
 
         private readonly string scopeName;
+
         private readonly string member;
+
         private readonly string path;
 
         public static LogLevel Level => LogLevel.Trace;
 
         public static LogCategory Category => LogCategory.Technical | LogCategory.Business;
 
-        public static Action<LogCategory, LogLevel, string> LogMethod;
+        public static Action<LogCategory, LogLevel, LogScope, string> LogMethod;
 
-        private LogScope(string scopeName, string member, string path)
+        public static Func<string> IdFactory { get; set; } = () => (DateTime.UtcNow.Ticks - 636818976000000000).ToString("X", CultureInfo.InvariantCulture);
+
+        private LogScope(string scopeName, string member, string path, LogScope parent = null)
         {
             this.scopeName = scopeName;
             this.member = member;
             this.path = path;
-            LogMethod?.Invoke(LogCategory.Technical, LogLevel.Trace, $"Starting scope {scopeName} in member {member} of {path}.");
+
+            this.IdStack = parent?.IdStack ?? new ConcurrentStack<string>();
+
+            var newId = LogScope.IdFactory();
+            this.IdStack.Push($"{parent?.Id}/{newId.Substring(newId.Length - 4)}");
+
+            LogMethod?.Invoke(LogCategory.Technical, LogLevel.Trace, this, $"Starting scope {scopeName} in member {member} of {path}.");
         }
+
+        /// <summary>
+        /// Gets the hierarchical ID of the scope.
+        /// </summary>
+        public string Id => this.IdStack.TryPeek(out var id) ? id : "0000";
+
+        public ConcurrentStack<string> IdStack { get; private set; }
 
         public void Dispose()
         {
             var ms = (DateTime.UtcNow - this.start).TotalMilliseconds;
-            LogMethod?.Invoke(LogCategory.Technical, LogLevel.Trace, $"Finished scope {this.scopeName} in member {this.member} of {this.path} - took {ms}ms.");
+            LogMethod?.Invoke(LogCategory.Technical, LogLevel.Trace, this, $"Finished scope {this.scopeName} in member {this.member} of {this.path} - took {ms}ms.");
         }
 
         public static LogScope MethodStart([CallerMemberName] string member = "", [CallerFilePath] string path = "")
@@ -43,9 +61,14 @@ namespace Sem.Tools.Logging
             return new LogScope(scopeName, member, path);
         }
 
+        public static LogScope Create(string scopeName, LogScope parent, [CallerMemberName] string member = "", [CallerFilePath] string path = "")
+        {
+            return new LogScope(scopeName, member, path, parent);
+        }
+
         public void Log(string message)
         {
-            LogMethod?.Invoke(LogCategory.Technical, LogLevel.Trace, message);
+            LogMethod?.Invoke(LogCategory.Technical, LogLevel.Trace, this, message);
         }
 
         public ValueTask DisposeAsync()
